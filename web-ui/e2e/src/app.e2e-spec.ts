3f40e1703810e4db7e0f3d7bd94c540f21a85c3f
Igor Amidzic <amidzicigor46@gmail.com>
2021-05-11 23:13:33 -0400

    Commit message...

--- web-ui/e2e/src/app.e2e-spec.ts
+++ web-ui/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('wealth-tracker app is running!');
+    expect(await page.getTitleText()).toEqual('wealthtracker app is running!');
   });
 
   afterEach(async () => {
--- web-ui/karma.conf.js
+++ web-ui/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/wealth-tracker'),
+      dir: require('path').join(__dirname, './coverage/wealthtracker'),
       subdir: '.',
       reporters: [
         { type: 'html' },
